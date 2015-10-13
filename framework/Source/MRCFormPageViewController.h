88d34508766acecc2ed2792d53cc7fe13646a19a
Gabor Nagy <gabor.nagy@shellshaper.com>
2015-10-13 10:04:08 +0200

    Finishing Form View Controller - Error handling

---
+++ framework/Source/MRCFormPageViewController.h
@@ -0,0 +1,35 @@
+//
+//  MRCFormPageViewController.h
+//  MrCoin iOS SDK
+//
+//  Created by Gabor Nagy on 08/10/15.
+//  Copyright © 2015 MrCoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MRCTextInput.h"
+#import "MRCAPI.h"
+
+@class MRCFormViewController;
+@class MRCTextInput;
+
+@interface MRCFormPageViewController : UIViewController <MRCTextInputDelegate>
+
+@property (nonatomic) id object;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
+@property (weak, nonatomic) IBOutlet UIButton *prevButton;
+//
+- (MRCFormViewController*)formController;
+
+- (IBAction)nextPage:(id)sender;
+- (IBAction)previousPage:(id)sender;
+- (void)nextPage:(id)sender withObject:(id)object;
+- (void)previousPage:(id)sender withObject:(id)object;
+
+- (void)showError:(NSString*)title message:(NSString*)message textInput:(MRCTextInput*)textInput;
+- (void)showErrorPopup:(NSString*)title message:(NSString*)message;
+- (void)showErrorTextInput:(MRCTextInput*)textInput error:(NSString*)title;
+
+- (MRCAPI *)api;
+
+@end
