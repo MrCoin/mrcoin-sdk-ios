fd1b2ccabcf1d6b802a2e639a8fcf0202012d5ed
Gabor Nagy <gabor.nagy@shellshaper.com>
2015-10-17 11:29:02 +0200

    Unit Test Cases
    - Formatter tests
    - API request header tests

--- framework/UnitTests/MrCoinTests.m
+++ framework/UnitTests/MrCoinTests.m
@@ -7,8 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "MRCVerificationCode.h"
+#import "MRCPhoneData.h"
+#import "MRCEmailData.h"
+#import "MRCAPI.h"
+#import "MrCoin.h"
 
-@interface MrCoinTests : XCTestCase
+@interface MrCoinTests : XCTestCase <MrCoinDelegate>
 
 @end
 
@@ -24,16 +29,69 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testAPIRequest {
+    [[MrCoin sharedController] setDelegate:self];
+    MRCAPI *api = [[MRCAPI alloc] init];
+    [[MrCoin settings] setWalletPrivateKey:@"PRIVATE"];
+    [[MrCoin settings] setWalletPublicKey:@"PUBLIC"];
+
+    NSURLRequest *r = [api requestMethod:@"authenticate" parameters:@"{@\"some\":@\"thing\"}" HTTPMethod:@"POST"];
+    NSString *nonce = [r valueForHTTPHeaderField:@"X-Mrcoin-Api-Nonce"];
+    NSString *sign = [r valueForHTTPHeaderField:@"X-Mrcoin-Api-Signature"];
+    NSString *validString = [NSString stringWithFormat:@"%@POST/api/v1/authenticate{@\"some\":@\"thing\"}",nonce];
+
+    XCTAssertEqualObjects([r valueForHTTPHeaderField:@"X-Mrcoin-Api-Pubkey"],@"PUBLIC");
+    XCTAssertEqualObjects(sign,validString);
+    XCTAssertEqualObjects([r valueForHTTPHeaderField:@"Accept"],@"application/vnd.api+json");
+    XCTAssertEqualObjects([r valueForHTTPHeaderField:@"Content-Type"],@"application/vnd.api+json");
+    XCTAssertTrue([r valueForHTTPHeaderField:@"HTTP_ACCEPT_LANGUAGE"]);
+    XCTAssertEqualObjects([r valueForHTTPHeaderField:@"HTTP_ACCEPT_LANGUAGE"],@"en");
+    
+    api.language = @"hu";
+    r = [api requestMethod:@"authenticate" parameters:nil HTTPMethod:@"POST"];
+    XCTAssertEqualObjects([r valueForHTTPHeaderField:@"HTTP_ACCEPT_LANGUAGE"],@"hu");
+
+}
+- (NSString *)requestSignatureFor:(NSString *)message privateKey:(NSString *)privateKey
+{
+    return message;
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testVerificationValidator {
+    MRCVerificationCode *formatter = [[MRCVerificationCode alloc] init];
+    XCTAssertFalse([formatter isValid:@"∂ß´¥"]); // Invalid chars
+    XCTAssertTrue(formatter.minimumLength == 4,@"[MRCVerificationCode minimumLength]");
+    XCTAssertTrue(formatter.maximumLength == 4,@"[MRCVerificationCode maximumLength]");
+    XCTAssertFalse([formatter isValid:@""]);
+    XCTAssertFalse([formatter isValid:@"12345"]);
+    XCTAssertFalse([formatter isValid:@"12345"]);
+    XCTAssertTrue([formatter isValid:@"1234"]);
+}
+- (void)testPhoneValidator {
+    MRCPhoneData *formatter = [[MRCPhoneData alloc] init];
+    [formatter setCountryCode:@"hu"];
+    XCTAssertFalse([formatter isValid:@"∂ß´¥ƒ∆˚ç˙@˚∂¨¥ß.¨¥"]); // Invalid chars
+    XCTAssertFalse([formatter isValid:@"+36112"]); // TOO SHORT
+    XCTAssertFalse([formatter isValid:@"+36"]); // EMPTY
+    XCTAssertFalse([formatter isValid:@"+363012345678"]); // TOO LONG
+    XCTAssertTrue([formatter isValid:@"+36301234567"]);
+    XCTAssertTrue([formatter isValid:@"+3611234567"]);
+}
+- (void)testPhoneFormatter {
+    MRCPhoneData *formatter = [[MRCPhoneData alloc] init];
+    [formatter setCountryCode:@"hu"];
+    XCTAssertEqualObjects([formatter format:@"+3611234567"], @"+36 1 123 4567");
+    XCTAssertEqualObjects([formatter unformat:@"+36 1 123 4567"], @"+3611234567");
+    XCTAssertEqualObjects([formatter format:@"+36207654321"], @"+36 20 765 4321");
+    XCTAssertEqualObjects([formatter unformat:@"+36 20 765 4321"], @"+36207654321");
+}
+- (void)testEmailValidator {
+    MRCEmailData *formatter = [[MRCEmailData alloc] init];
+    XCTAssertFalse([formatter isValid:@"kovacs@jancsi@politikus.hu"]); // Invalid chars
+    XCTAssertFalse([formatter isValid:@"∂ß´¥ƒ∆˚ç˙@˚∂¨¥ß.¨¥"]); // Invalid chars
+    XCTAssertFalse([formatter isValid:@"k@a.h"]); // Short
+    XCTAssertFalse([formatter isValid:@"sergej@oil.r"]); // Invalid domain
+    XCTAssertTrue([formatter isValid:@"ivan.ivanovics.gorkij@kiosk.ru"]);
 }
 
 @end
